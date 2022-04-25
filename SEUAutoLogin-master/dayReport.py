ce9a692cfd1f837e5bb86daa51b5032b479f8929
Mr.Wang <56351667+740402059@users.noreply.github.com>
2022-04-25 17:14:07 +0800

    Add files via upload

---
+++ SEUAutoLogin-master/dayReport.py
@@ -0,0 +1,167 @@
+# encoding=utf-8
+import argparse
+import datetime
+import json
+import random
+import pytz
+
+from bs4 import BeautifulSoup
+from login import login
+
+# 加载全局配置
+with open("./config.json", "r", encoding="utf-8") as f:
+    configs = f.read()
+configs = json.loads(configs)
+isDebug = False
+
+
+def load_params(ss, mode):
+    '''合并填报参数'''
+    json_form = get_report_data(ss)  # 获取昨日填报信息
+
+    params = {
+        "DZ_JSDTCJTW": 36.5,
+        "DZ_DBRQ": "%Y-%m-%d",
+        "CZRQ": "%Y-%m-%d %H:%M:%S",
+        "CREATED_AT": "%Y-%m-%d %H:%M:%S",
+        "NEED_CHECKIN_DATE": "%Y-%m-%d"
+    }
+    params["DZ_JSDTCJTW"] = 36 + random.randint(1, 10) / 10
+    if mode != '':
+        try:
+            local = configs['dailyReport'][mode]
+            params.update(local)
+        except Exception:
+            print("【加载本地配置失败，使用昨日信息进行填报】")
+    else:
+        print("【使用昨日信息进行填报】")
+
+    # get time
+    today = datetime.datetime.now()
+    yesterday = today - datetime.timedelta(days=1)
+
+    today_list = ['CZRQ', 'CREATED_AT', 'NEED_CHECKIN_DATE']
+    yesterday_list = ['DZ_DBRQ']
+    for key in params.keys():
+        # 填充日期
+        if key in yesterday_list:
+            params[key] = yesterday.strftime(params[key])
+        elif key in today_list:
+            params[key] = today.strftime(params[key])
+        json_form[key] = params[key]
+    # print(params)
+    return json_form
+
+
+def doReport(session, mode=''):
+    """
+    session: 已登录的 requests.session 对象
+    mode:  填报配置 home, school 等，(留空使用昨天的填报信息)，可自行修改 config.json 文件
+    """
+    # 进入填报页面（获取sessionid）
+    session.get("http://ehall.seu.edu.cn/appShow?appId=5821102911870447")
+
+    url = 'http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/mobile/dailyReport/T_REPORT_EPIDEMIC_CHECKIN_SAVE.do'
+
+    json_form = load_params(session, mode)
+
+    res = session.post(url, data=json_form)
+    try:
+        if json.loads(res.text)['datas']['T_REPORT_EPIDEMIC_CHECKIN_SAVE'] == 1:
+            print("填报成功！")
+        else:
+            print("填报失败！")
+    except Exception:
+        soup = BeautifulSoup(res.text, "html.parser")
+        tag = soup.select('.underscore.bh-mt-16')
+        if len(tag) > 1:
+            print(tag[0].text.replace('\n', ''))
+        else:
+            print(res.text)
+        print("填报失败！")
+
+
+# 获取昨日填报信息
+def get_report_data(ss):
+    # 进入填报页面（获取sessionid）
+    ss.get("http://ehall.seu.edu.cn/appShow?appId=5821102911870447")
+    latest_url = "http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/modules/dailyReport/getLatestDailyReportData.do"
+    wid_url = "http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/mobile/dailyReport/getMyTodayReportWid.do"
+    userinfo_url = "http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/api/base/getUserDetailDB.do"
+    last_res = ss.get(latest_url)
+    wid_res = ss.get(wid_url)
+    userinfo_res = ss.post(userinfo_url)
+    try:
+        tempFormData = {}
+        userInfo = json.loads(userinfo_res.text)['data']
+        # 载入当天填报模板
+        try:
+            wid_data = json.loads(
+                wid_res.text)['datas']['getMyTodayReportWid']['rows'][0]
+            tempFormData.update(wid_data)
+        except Exception:
+            print('【getMyTodayReportWid FAILED】')
+            # raise       # 经测试：wid为空无影响
+
+        # 载入昨日填报信息
+        try:
+            last_report = json.loads(
+                last_res.text)['datas']['getLatestDailyReportData']['rows'][0]
+            tempFormData.update(last_report)
+        except Exception:
+            print('getLatestDailyReportData FAILED】')
+            raise
+
+        # 载入用户信息
+        tempFormData['USER_ID'] = configs['user']['cardnum']
+        # tempFormData['PHONE_NUMBER'] = userInfo['PHONE_NUMBER']
+        tempFormData['IDCARD_NO'] = userInfo['IDENTITY_CREDENTIALS_NO']
+        tempFormData['GENDER_CODE'] = userInfo['GENDER_CODE']
+
+        # tempFormData['CLASS_CODE'] = userInfo['CLASS_CODE']
+        # tempFormData['CLASS'] = userInfo['CLASS']
+        tempFormData['RYSFLB'] = userInfo['RYSFLB']
+        tempFormData['USER_NAME'] = userInfo['USER_NAME']
+        tempFormData['DEPT_CODE'] = userInfo['DEPT_CODE']  # 学院编号
+        tempFormData['DEPT_NAME'] = userInfo['DEPT_NAME']
+        if isDebug:
+            print(tempFormData)
+    except Exception as e:
+        print(e)
+        print("【获取填报信息失败，请手动填报】")
+        exit()
+    return tempFormData
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Test for argparse')
+    parser.add_argument(
+        '--config', '-c', help='采用的配置名称 如 school, home', default='')
+    parser.add_argument(
+        '--force', '-f', help='15:00 后是否仍然填报', action='store_true')
+    parser.add_argument(
+        '--debug', '-d', help='显示调试信息', action='store_true')
+    parser.add_argument(
+        '--user', '-u', help='一卡通号', default='')
+    parser.add_argument(
+        '--password', '-p', help='密码', default='')
+
+    args = parser.parse_args()
+    isDebug = args.debug
+
+    # 覆盖账号信息
+    if args.user != '' and args.password != '':
+        configs['user']['cardnum'] = args.user
+        configs['user']['password'] = args.password
+
+    today = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
+    print(today)
+    if today.hour >= 15:  # 超过填报时间
+        if args.force:
+            print("【超过填报时间，但继续填报】")
+        else:
+            print("【超过填报时间！放弃填报】")
+            exit()
+    ss = login(configs['user']['cardnum'], configs['user']['password'])
+    if ss:
+        doReport(ss, args.config)
---
+++ SEUAutoLogin-master/readme.md
@@ -0,0 +1,183 @@
+# SEU Auto Login
+
+东南大学信息门户自动登录，采用requests库，获取登录session，无需浏览器依赖，全平台通用。**目前已添加GitHub Action自动部署功能，无需服务器，一键fork部署！！！🎉**
+
+**本项目仅供学习使用**
+
+- 🌡**自动每日健康上报** [![daily report](https://github.com/luzy99/SEUAutoLogin/actions/workflows/daily-report.yml/badge.svg)](https://github.com/luzy99/SEUAutoLogin/actions/workflows/daily-report.yml)
+- 💯**绩点计算**
+
+## 📋目录结构
+
+```sh
+│  config.json	# 配置文件
+│  dayReport.py	# 每日上报脚本
+│  gpa.py		# gpa计算脚本
+│  ids-encrypt.js	# SEU 身份认证AES加密算法
+│  ids_encrypt.py	# js2py调用js代码
+│  login.py		# 登录
+│  readme.md
+│  requirements.txt
+```
+
+## 🛠安装依赖
+
+开发环境：
+python 3.7
+
+依赖包
+
+```
+requests~=2.23.0
+Js2Py~=0.70
+beautifulsoup4~=4.9.3
+```
+
+安装依赖，请执行：
+
+```sh
+pip install -r requirements.txt
+```
+
+## 📐功能说明（本地部署）
+
+**请先在`config.json`中填写好一卡通号和密码**
+
+```json
+    "user": {
+        "cardnum": "你的一卡通号",
+        "password": "登录密码"
+    },
+```
+
+### 自动每日健康上报
+
+- 运行方式，双击或命令行输入运行
+
+```
+./dayReport.py
+```
+
+默认采用昨日信息填报。
+
+- （**可选，不推荐**）手动设置填报信息
+
+  > 出现问题概不负责（逃
+
+  修改`config.json`文件，修改或添加配置，以下配置将覆盖昨日信息
+
+  ```json
+  "dailyReport": {
+          "school": {
+              "DZ_JSDTCJTW": 36.5,	// 体温
+              "DZ_SZWZLX": "002",		// 在校内
+              "DZ_SZWZLX_DISPLAY": "在校内",
+              "LOCATION_PROVINCE_CODE": "",
+              "LOCATION_PROVINCE_CODE_DISPLAY": "",
+              "LOCATION_CITY_CODE": "",
+              "LOCATION_CITY_CODE_DISPLAY": "",
+              "LOCATION_COUNTY_CODE": "",
+              "LOCATION_COUNTY_CODE_DISPLAY": "",
+              "DZ_SZXQ": "002",
+              "DZ_SZXQ_DISPLAY": "九龙湖校区"
+          },
+      // 若不清楚区域编号请勿随意修改！！！！
+          "home": {
+              "DZ_JSDTCJTW": 36.555,
+              "DZ_SZWZLX": "003",		// 在校外(在南京以外)
+              "DZ_SZWZLX_DISPLAY": "在校外(在南京以外)",
+              "LOCATION_PROVINCE_CODE": "110100",		// 省份编号
+              "LOCATION_PROVINCE_CODE_DISPLAY": "北京市",
+              "LOCATION_CITY_CODE": "110100",		// 城市编号
+              "LOCATION_CITY_CODE_DISPLAY": "北京市",
+              "LOCATION_COUNTY_CODE": "110101",	// 区编号
+              "LOCATION_COUNTY_CODE_DISPLAY": "市辖区",
+              "DZ_SZXQ": "",
+              "DZ_SZXQ_DISPLAY": ""
+          }
+      ...
+  }
+  ```
+
+  带配置启动方式：
+
+  ```sh
+  ./dayReport.py -c home
+  ```
+
+### GPA计算
+
+> 按照学生手册公式计算，仅供参考
+
+- 运行方式
+
+  ```sh
+  ./dayReport.py -t 学期号
+  ```
+
+  ```sh
+  usage: gpa.py [-h] [--term TERM]
+  
+  Test for argparse
+  
+  optional arguments:
+    -h, --help            show this help message and exit
+    --term TERM, -t TERM  学期 20(计算20-21学年所有学期), 2020-2021-1(指定某一学期)
+  examples:
+  	
+  ```
+
+- examples
+
+  ```sh
+  # 一个学年
+  ❯ ./gpa.py -t 19
+  XXX  登陆成功！
+  成功读取 50 门课程成绩
+  必修总学分：90.000000, 累计绩点：4.8
+  Terms in  ['2019-2020-1', '2019-2020-2', '2019-2020-3', '2019-2020-4']
+  学年必修总学分：46.000000, 学年绩点：4.522826
+  
+  # 指定学期
+  ❯ ./gpa.py -t 2020-2021-1
+  XXX  登陆成功！
+  成功读取 50 门课程成绩
+  必修总学分：90.000000, 累计绩点：4.8
+  Terms in  ['2020-2021-1']
+  学年必修总学分：10.000000, 学年绩点：4.629412
+  ```
+
+
+---
+
+## Github Action自动部署！
+
+1. 先`fork`本仓库
+
+   ![image-20210405124959083](https://cdn.jsdelivr.net/gh/luzy99/cdn@latest/img/20210405130131.png)
+
+2. 进入您账号下的`SEUAutoLogin`仓库，点击`Settings`，找到`Secrets`，点击 `New repository secret`新建仓库密钥
+
+   ![image-20210405124603956](https://cdn.jsdelivr.net/gh/luzy99/cdn@latest/img/20210405130137.png)
+
+3. 新建2个key：`CARD_NUM`, `PASSWORD`，输入你的**一卡通号**和**密码**
+
+   ![image-20210405125326461](https://cdn.jsdelivr.net/gh/luzy99/cdn@latest/img/20210405130141.png)
+
+   ​	如下图：
+
+   ![image-20210405125412870](https://cdn.jsdelivr.net/gh/luzy99/cdn@latest/img/20210405130145.png)
+
+4. 至此已经设置完成（默认每天北京时间**8点31分**上报），可以先手动运行一下试试。
+
+   步骤如下图：
+
+   ![image-20210405125843879](https://cdn.jsdelivr.net/gh/luzy99/cdn@latest/img/20210405130151.png)
+
+5. 运行结果
+
+   ![image-20210405130018830](https://cdn.jsdelivr.net/gh/luzy99/cdn@latest/img/20210405130155.png)
+
+   
+
+### ⚠如果无法自动执行，在readme文件中随便加个回车，再commit，push即可
